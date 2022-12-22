571ce8c0fac48dce456ed78a4a1b2170eec0270f
TiagoUniverse <tiagocesar68@gmail.com>
2022-12-21 21:32:44 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,47 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import FirstPage from './pages/FirstPage';
+import SecondPage from './pages/SecondPage';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="FirstPage">
+        <Stack.Screen
+          name="FirstPage"
+          component={FirstPage}
+          options={{
+            title: 'Primeira Tela',
+            headerStyle: {
+              backgroundColor: '#0000FF',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="SecondPage"
+          component={SecondPage}
+          options={{
+            title: 'Segunda Tela',
+            headerStyle: {
+              backgroundColor: '#0000FF',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
